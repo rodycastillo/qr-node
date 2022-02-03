59c61b0dcca6fc85fc06c0bee7913f51777d5c02
Carlito <rody.castillo.tacuri@gmail.com>
2022-02-03 11:58:31 -0500

    feat: add Commitizen

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
-// const fs = require('fs')
-// const qrcode = require('qrcode')
-const fetch = require('node-fetch')
+const { json } = require("express")
+const express = require("express")
+const app = express()
+const fs = require('fs')
+const qrcode = require('qrcode')
 const request = require('request')
+// const htmlToImage = require('html-to-image');
+
+app.use(express.json())
+
+const url = 'https://wasipetapp.com/api/qrs2021';
 
 
-// const url = 'https://wasipetapp.com/api/qrs2021';
+const run = async () => {
 
-const run = () => {
-
-    request.get(url, (err, resp, body) =>{
+      request.get(url, (err, resp, body) =>{
         if(err) {
             return console.error(err);
         }
-        console.dir(JSON.parse(body))
+        const data = JSON.parse(body)
+        // console.log(data);
+        data.forEach((qr, i) => {
+            const QR =  qrcode.toDataURL(`id:${qr._id},points:${qr.points}`);
+            const htmlContent = `
+            <div style="display: flex; flex-direction: column; justify-content: center; align-items: center;">
+                <h2>QR code</h2>
+                <br />
+                <img src="${QR}"/>
+            </div>
+            `;
+            fs.writeFileSync(`./img/00${i+1}.html`, htmlContent)
+        })
     })
-    // const QR = await qrcode.toDataURL(url);
-    // const htmlContent = `
-    // <div style="display: flex; flex-direction: column; justify-content: center; align-items: center;">
-    //     <h2>QR code</h2>
-    //     <br />
-    //     <img src="${QR}"/>
-    // </div>
-    // `;
-    // fs.writeFileSync(`./{QR}.html`, htmlContent)
+    
+    
 }
 run()
